b578fadf98148fd00f02243678ef35fde73386ea
TP <tiagomourabrandao@gmail.com>
2023-08-18 15:51:50 -0300

    Adding website hosting

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/labstack/echo/v5"
-    "github.com/pocketbase/pocketbase"
-    "github.com/pocketbase/pocketbase/core"
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
     app := pocketbase.New()
+
+    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+        e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+        return nil
+    })
+
+    if err := app.Start(); err != nil {
+        log.Fatal(err)
+    }
 
     app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/hello/:name", func(c echo.Context) error {
